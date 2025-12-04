2faa68bb4684c3137bb4f0d0825b3e94bcc1fc85
Clare Sloggett <claresloggett@gmail.com>
2025-12-04 23:31:35 +1100

    Keep cursor position when moving cards to the backlog

--- app/app.py
+++ app/app.py
@@ -159,7 +159,10 @@ class ABTerm(App):
         self.card_client.update_card_sprint(self.current_card_id, f"{backlog}", self.sprint_client)
         # Refresh the cards panel
         if self.current_sprint_id is not None:
+            # Keep the current cursor position, by index, irrespective of card changes
+            row_index = self.cards_panel.table.cursor_row
             self.cards_panel.get_cards(self.current_sprint_id)
+            self.cards_panel.table.move_cursor(row=row_index)
         self.command_state = CommandState.NORMAL
         self.refresh_bindings()
     
